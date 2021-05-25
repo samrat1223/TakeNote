be7bb6318031f17643d6f4f44ffc7fdfa97a3ed9
samrat1223 <samratganguly37@gmail.com>
2021-05-25 09:45:44 +0530

    Done-UnDone and Delete functionslity for a todo added

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,27 @@
-import { ListItem, ListItemText } from '@material-ui/core'
+import { ListItem, ListItemText,Button } from '@material-ui/core'
 import React from 'react'
+import { db } from './firebase_config'
 
 export default function TodoListItem({todo,inprogress,id}) {
+
+    function toggleInProgress(){
+        db.collection("addnotes").doc(id).update({
+            inprogress: !inprogress
+        });
+    }
+
+    function deleteTodo(){
+        db.collection("addnotes").doc(id).delete();
+    }
+
     return (
-        <div>
+        <div style={{display:"flex"}}>
            <ListItem>
                <ListItemText primary={todo} secondary={inprogress ? "InProgress" : "Hurray!!Done"} />
            </ListItem>
+
+           <Button onClick={toggleInProgress}>{inprogress ? "Done" : "UnDone"}</Button>
+           <Button onClick={deleteTodo}>X</Button>
         </div>
     )
 }
