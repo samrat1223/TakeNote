cde2d9542aad95fa9629d283da6f4ea5972daf2f
Samrat Ganguly <60837980+samrat1223@users.noreply.github.com>
2021-05-25 10:41:26 +0530

    Asterisked application credentials

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvO0rQXbztbU2YtRhI0ld7R3reV8vyb5w",
-  authDomain: "takenote-b6731.firebaseapp.com",
-  //databaseURL: "https://takenote-b6731.firebaseio.com",
-  projectId: "takenote-b6731",
-  storageBucket: "takenote-b6731.appspot.com",
-  messagingSenderId: "1067990029135",
-  appId: "1:1067990029135:web:86a261592d0b6cb29d6b3a"
+  apiKey: "****",
+  authDomain: "******",
+  //databaseURL: "****",
+  projectId: "****",
+  storageBucket: "****",
+  messagingSenderId: "***",
+  appId: "*****"
 };
 
 
